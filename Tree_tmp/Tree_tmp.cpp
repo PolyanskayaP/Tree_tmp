38ee10ca6d262eb565bfaa31af6791990f243b0e
Полина Полянская <ppolina353@yandex.ru>
2022-01-02 17:28:35 +0300

    Элементы записываются в деревья теперь

--- Tree_tmp/Tree_tmp.cpp
+++ Tree_tmp/Tree_tmp.cpp
@@ -16,64 +16,117 @@ struct node {
     int mark;
 };
 
-int findNode(int value, int k, node* A, int& size) { //возвр индекс вершины-родителя
+void findNode(int value, /**/int k, node* A, /*mojet size*/int& size) { //возвр индекс вершины-родителя
     //k = k - 1;
-    if (size == 0) {
-        
-     /*   A = (node*)realloc(A, sizeof(node) * (++size));
+    //k = k + 1;
+    if (size == 0) {                     //zapih znachh  addnafi   !!!!!!!
+    /*   A = (node*)realloc(A, sizeof(node) * (++size));
         A[k].left_child = -1;
         A[k].mark = 0; //virtual
         A[k].name = size + 65;
         A[k].right_sibling = -1;
         A[k].znach = -1;
         A[k].index = size - 1;   */
-        return -1; //zanuli!!!!   k - index tekus..?
-    }
-    int indLeft = A[k].left_child;
-    if (indLeft == -1) {
-        A = (node*)realloc(A, sizeof(node) * (++size));
-        A[size - 1].left_child = -1;
-        A[size - 1].mark = 0; //virtual
-        A[size - 1].name = size - 1 + 65;
-        A[size - 1].right_sibling = -1;
-        A[size - 1].znach = -1;
-        A[size - 1].index = size - 1;
+   //     return -1; //zanuli!!!!   k - index tekus..?
 
-        A[k].left_child = A[size - 1].index;
+        A[size].index = size;
+        A[size].left_child = -1;
+        A[size].mark = 1;
+        A[size].name = size + 65;
+        A[size].right_sibling = -1;
+        A[size].znach = value; 
 
-        indLeft = A[k].left_child;
+        size++;
+        return;
     }
-    int indRight = A[indLeft].right_sibling;
 
-    if (value < A[k].znach and A[indLeft].mark == 1 ) {
-        findNode(value, indLeft, A, size);
-    }
-    else 
-    if (value >= A[k].znach and indRight != -1) {
-        //if (indRight != -1) {//left?
-            findNode(value, indRight, A, size);
-        //}
-        //else {
-        //    ret
-        //}
+    if (value < A[k].znach) {
+        if (A[k].left_child == -1) {
+            A[size].index = size;
+            A[size].left_child = -1;
+            A[size].name = size + 65;
+            A[size].znach = value;
+            A[size].right_sibling = -1;
+            A[size].mark = 1;
+
+            A[k].left_child = size;
+            size++;
+        }
+        else
+            if (A[A[k].left_child].mark == 1) {
+                findNode(value, A[k].left_child, A, size);
+        }
+        else
+            if (A[A[k].left_child].mark == 0) {
+                A[A[k].left_child].znach = value;
+                A[A[k].left_child].mark = 1;
+
+                A[k].left_child = A[A[k].left_child].index;
+            }
     }
     else {
-        return k; //roditel' 
+        if (A[k].left_child == -1) {
+            A[k].left_child = size;
+
+            A[size].index = size;
+            A[size].left_child = -1;
+            A[size].name = size + 65;
+            A[size].znach = -1;
+            int lsize = size;//
+            A[lsize].right_sibling = ++size;
+            A[lsize].mark = 0;
+
+            int rig = A[lsize].right_sibling;
+            A[rig].index = rig;
+            A[rig].left_child = -1;
+            A[rig].name = rig + 65;
+            A[rig].znach = -1;
+            A[rig].right_sibling = -1;
+            A[rig].mark = 0;
+
+            size++;
+        }
+        if (A[A[k].left_child].right_sibling == -1) {
+            A[A[k].left_child].right_sibling = size;
+
+            A[size].index = size;
+            A[size].left_child = -1;
+            A[size].name = size + 65;
+            A[size].znach = -1;
+            A[size].right_sibling = -1;
+            A[size].mark = 0;
+
+            size++;
+        }
+
+        int indRight = A[A[k].left_child].right_sibling;
+        if (A[indRight].mark == 1) {
+            findNode(value, indRight, A, size);
+        }
+        else {
+            /////if A[indRight].mark == 0;
+            //A[indRight].index = indRight;
+            //A[indRight].name = indRight + 65;
+            A[indRight].znach = value;
+            A[indRight].mark = 1;
+        }
     }
-   //}
+    return;
 }
 
-void add(int value, node* A, int& size) {  //prover' size!
-    int i = size; //-1 
-    int indParent = findNode(value, i, A, size); //a  ecli -1????? to est'[0]   
+/*void add(int value, node* A, int& size) {  //prover' size!
+    int i = size; 
+    int indParent = findNode(value, 0, A, size); //a  ecli -1????? to est'[0]   
     if (indParent == -1) {
-        A = (node*)realloc(A, sizeof(node) * (++size));
-        A[i].index = i;
-        A[i].left_child = -1;
-        A[i].mark = 1;
+    //    A = (node*)realloc(A, sizeof(node) * (++size));
+        A[i].index = i;  
+        A[i].left_child = -1; //cout << "zahsel!"<< size<<i;  
+        A[i].mark = 1; //cout << A[i].left_child;
         A[i].name = i + 65;
         A[i].right_sibling = -1;
         A[i].znach = value;
+        size++;
+        return; 
     }
 
     if (value < A[indParent].znach) { //ya,rod,brat
@@ -111,52 +164,55 @@ void add(int value, node* A, int& size) {  //prover' size!
         A[A[indParent].left_child].right_sibling = i;
 
     }
-}
+}*/
 
 int main()
 {
-    int i;
-    int forA[v] = { 5,3,6,7,4,2,1 };
-    int forB[v] = { 2,9,8,10,1,0,3 };
-    /*std::cout << "Hello World!\n";
-    stroka* B = new stroka[10];//how much?
-    delete[] B; */
-
+    int i, j;
+    int forA[v] = { 50,30,60,70,40,20,10 };
+    int forB[v] = { 22,99,88,10,11,0,33 };
     int sizeA = 0; 
-    node* A = 0; 
-
-  /*  int sizeB = 0;
-    node* B = 0;
-
-    int sizeC = 0;
-    node* C = 0;
+    int sizeB = 0;
+    //node* A = (node *) malloc (sizeof(node));
+    node A[v*v];//*2
+    node B[v * v];
 
     for (i = 0; i < v; i++) {
-        A = (node*)realloc(A, sizeof(node) * (++sizeA));  
-        A[i].znach = forA[i];
-        A[i].name = i+65; //
-        A[i].index = i;
-
-        //cout << A[i].znach << " " << A[i].name << " ";
-    } //cout << endl;
-
-    for (i = 0; i < v; i++) {
-        B = (node*)realloc(B, sizeof(node) * (++sizeB));
-        B[i].znach = forB[i];
-        B[i].name = i + 65; //
-        B[i].index = i;
-
-        //cout << B[i].znach << " " << B[i].name << " ";
+        findNode(forA[i], 0, A, sizeA);
+        //add(forA[i], A, sizeA); cout <<"size="<< sizeA;
+        //cout <<" "<< A[i].znach << " " << sizeA << " ";
     }
 
-    */
-    for (i = 0; i < v; i++) {
-        
-        add(forA[i], A, sizeA);
-        //cout << A[i].znach << " ";
+    i = 0;  
+    j = 0; 
+    while (j < v) {
+        if (A[i].mark == 1) {
+            cout << A[i].znach << " ";
+            j++;
+        }
+        i++;
     }
 
-    free(A);
+    cout << endl;
+    cout << endl;
+
+    for (i = 0; i < v; i++) {
+        findNode(forB[i], 0, B, sizeB);
+        //add(forA[i], A, sizeA); cout <<"size="<< sizeA;
+        //cout <<" "<< A[i].znach << " " << sizeA << " ";
+    }
+
+    i = 0;
+    j = 0;
+    while (j < v) {
+        if (B[i].mark == 1) {
+            cout << B[i].znach << " ";
+            j++;
+        }
+        i++;
+    }
+
+    //free(A);//
  //   free(B);
     return 0; 
 }
@@ -190,3 +246,37 @@ int main()
     delete[] C;
     delete[] B;   */
     //delete[] A;
+
+    //cout << A[k].left_child;   //find
+    /*int indLeft = A[k].left_child;
+    if (indLeft == -1) {
+        A = (node*)realloc(A, sizeof(node) * (++size));
+        A[size - 1].left_child = -1;
+        A[size - 1].mark = 0; //virtual
+        A[size - 1].name = size - 1 + 65;
+        A[size - 1].right_sibling = -1;
+        A[size - 1].znach = -1;
+        A[size - 1].index = size - 1;
+
+        A[k].left_child = A[size - 1].index;
+
+        indLeft = A[k].left_child;
+    }
+    int indRight = A[indLeft].right_sibling;
+
+    if (value < A[k].znach and A[indLeft].mark == 1 ) {
+        findNode(value, indLeft, A, size);
+    }
+    else
+    if (value >= A[k].znach and indRight != -1) {
+        //if (indRight != -1) {//left?
+            findNode(value, indRight, A, size);
+        //}
+        //else {
+        //    ret
+        //}
+    }
+    else {
+        return k; //roditel'
+    }
+   //}*/
